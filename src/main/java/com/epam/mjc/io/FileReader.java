fe254c3541f72fa965bd928558bc0310ed8f1992
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-11-01 17:07:33 +0200

    reading profile data

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+        StringBuilder data = new StringBuilder();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            while((ch = fileInputStream.read()) != -1) {
+                data.append((char) ch);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return getProfileFromString(data.toString());
+    }
+
+    public static Profile getProfileFromString(String data) {
+        String[] rows = data.split("\n");
+        String name = rows[0].split(": ")[1].trim();
+        String age = rows[1].split(": ")[1].trim();
+        String email = rows[2].split(": ")[1].trim();
+        String phone = rows[3].split(": ")[1].trim();
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
+    }
+
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/Profile.txt");
+        getDataFromFile(file);
     }
 }
